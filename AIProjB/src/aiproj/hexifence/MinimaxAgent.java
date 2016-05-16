26a04815d6916c014ba20ea346d66ce50fa54c02
wpan1 <wpan1@student.unimelb.edu.au>
2016-05-16 21:47:34 +1000

    added a better heuristic

--- AIProjB/src/aiproj/hexifence/MinimaxAgent.java
+++ AIProjB/src/aiproj/hexifence/MinimaxAgent.java
@@ -8,13 +8,13 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
+import java.util.Stack;
 
 public class MinimaxAgent implements Player{
 	public GameBoard gameBoard;
 	int pieceColor;
 	int oppPieceColor;
 	private static int MINIMAX_DEPTH = 10;
-	private static ArrayList<ArrayList<Integer>> connectingEdges = new ArrayList<ArrayList<Integer>>();
 	
 	@Override
 	public int init(int n, int p) {
@@ -36,35 +36,41 @@ public class MinimaxAgent implements Player{
 	
 	@Override
 	public Move makeMove(){
-//		// Return move if capturable
-//		int row = 0;
-//		for (ArrayList<Integer> move : gameBoard.getMoves()){
-//			Move mv = new Move(move.get(1),move.get(0),pieceColor);
-//			if (gameBoard.checkCapture(mv)) {
-//				gameBoard.update(mv);
-//				return mv;
-//			}
-//		}
-		if (gameBoard.totalMovesLeft > 23){
-			// Use minimax
-			int[] moveDet = minimax(5, pieceColor, Integer.MIN_VALUE, Integer.MAX_VALUE);
-			Move move = new Move(moveDet[2], moveDet[1], pieceColor);
-			gameBoard.update(move);
-			return move;
+		// Return move if capturable
+		int maxScore = Integer.MIN_VALUE;
+		Move maxMove = null;
+		// If more than ten moves left
+		if (gameBoard.totalMovesLeft > 10){
+			// Check all remaining moves
+			for (ArrayList<Integer> move : gameBoard.getMoves()){
+				// If anything is capturable, capture it
+				Move mv = new Move(move.get(1),move.get(0),pieceColor);
+				if (gameBoard.checkCapture(mv)) {
+					gameBoard.update(mv);
+					return mv;
+				}
+				// This is basically minimax for one level
+				gameBoard.update(mv);
+				int score = evaluateBoard2();
+				// Find the best move
+				if (score > maxScore){
+					maxScore = score;
+					maxMove = mv;
+				}
+				gameBoard.remove(new ArrayList<Integer>(move));
+			}
+			// Update board
+			gameBoard.update(maxMove);
+			return maxMove;
 		}
 		// When to start using minimax
-		else if (gameBoard.totalMovesLeft > 15){
+		else{
 			// Use minimax
-			int[] moveDet = minimax(7, pieceColor, Integer.MIN_VALUE, Integer.MAX_VALUE);
+			int[] moveDet = minimax(20, pieceColor, Integer.MIN_VALUE, Integer.MAX_VALUE);
 			Move move = new Move(moveDet[2], moveDet[1], pieceColor);
 			gameBoard.update(move);
 			return move;
 		}
-		// Use minimax
-		int[] moveDet = minimax(10, pieceColor, Integer.MIN_VALUE, Integer.MAX_VALUE);
-		Move move = new Move(moveDet[2], moveDet[1], pieceColor);
-		gameBoard.update(move);
-		return move;
 	}
 	
 	private int[] minimax(int depth, int currPieceColor, int alpha, int beta) {
@@ -76,7 +82,7 @@ public class MinimaxAgent implements Player{
 	    // Gameover or depth reached, evaluate score
 		if (gameBoard.totalMovesLeft == 0 || depth == 0){
 			if (gameBoard.totalMovesLeft == 0) currScore = evaluateBoard3();
-			else currScore = evaluateBoard3();
+			else currScore = evaluateBoard2();
 			return new int[]{currScore, bestRow, bestCol};
 		}
 		else{
@@ -157,57 +163,25 @@ public class MinimaxAgent implements Player{
 		}
 		return score;
 	}
-	
-	/**
-	 * Evaluation at the end of the game
-	 * @return
-	 */
-	private int evaluateBoard(){
-		if (gameBoard.blueCap > gameBoard.redCap)
-			return 1;
-		return -1;
-	}
 
 	/**
-	 * Ignore this
+	 * Evaluate chains
 	 * @return
 	 */
-	private int evaluateBoard2(){		
-		ArrayList<ArrayList<Hexagon>> links = new ArrayList<ArrayList<Hexagon>>();
-		ArrayList<Hexagon> tempLink = null;
-		// Iterate through moves
-		for (ArrayList<Integer> move : gameBoard.getMoves()){
-			if (gameBoard.hexagonMap.get(move).size() < 2) continue;
-			// Iterate through hexagons mapping to move
-			for (Hexagon hex : gameBoard.hexagonMap.get(move)){
-				// Iterate through links
-				linkloop : for (ArrayList<Hexagon> link : links){
-					// Iterate though hexagon in links
-					for (Hexagon hex2 : link){
-						if (hex.equals(hex2)){
-							tempLink = link;
-							break linkloop;
-						}
-					}
-				}
-			}
-			if (tempLink != null){
-				tempLink.removeAll(gameBoard.hexagonMap.get(move));
-				tempLink.addAll(gameBoard.hexagonMap.get(move));
-				tempLink = null;
-			}else{
-				links.add(new ArrayList<Hexagon>(gameBoard.hexagonMap.get(move)));
-			}
-
-		}
-		Collections.sort(links, new Comparator<ArrayList>(){
+	private int evaluateBoard2(){	
+		// Sort chain lengths
+		Collections.sort(gameBoard.chainList, new Comparator<ArrayList>(){
 		    public int compare(ArrayList a1, ArrayList a2) {
 		        return a1.size() - a2.size(); // assumes you want biggest to smallest
 		    }
 		});
-		boolean add = false;
+		// If odd move, add first
+		boolean add = true;
+		// If even move, subtract first
+		if (gameBoard.totalMovesLeft%2 == 0)
+			add = false;
 		int score = 0;
-		for (ArrayList<Hexagon> link : links){
+		for (ArrayList<Hexagon> link : gameBoard.chainList){
 			if (add) score += link.size();
 			else score -= link.size();
 			add = !add;
